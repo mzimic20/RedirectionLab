0deb4e0bd8c4224ba5886ccaa6499cf1d5da98c7
mzimic20 <>
2021-01-04 18:07:02 -0500

    pigLatinSimple() and pigLatin() complete

---
+++ PigLatin.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class PigLatin {
+
+  public static void main(String[] args) {
+    Scanner n = new Scanner( System.in );
+    String s, t;
+    while( n.hasNextLine() ) {
+      Scanner m = new Scanner( n.nextLine() );
+      while( m.hasNext() ) {
+        t = m.next();
+        System.out.print( pigLatin(t) + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static String pigLatinSimple(String s) {
+    s = s.toLowerCase();
+    char a;
+    if ( s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' || s.charAt(0) == 'o' || s.charAt(0) == 'u' ) {
+      return s + "hay";
+    }
+    else {
+      a = s.charAt(0);
+      return s.substring(1, s.length() ) + a + "ay";
+    }
+  }
+
+  public static String pigLatin(String s) {
+    s = s.toLowerCase();
+    if ( s.length() < 3) return pigLatinSimple(s);
+    String[] digraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    String start = "" + s.charAt(0) + s.charAt(1);
+    for(int i = 0; i < digraphs.length; i++) {
+      if ( digraphs[i].equals(start) ) {
+        return s.substring(2, s.length() ) + start + "ay";
+      }
+    }
+    return pigLatinSimple(s);
+  }
+
+}
